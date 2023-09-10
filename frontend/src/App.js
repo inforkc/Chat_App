892a351e5e2ff0e5cf2f1f81cff3e2b384207bf8
FirdausJawed <firdausj.ug20.ee@nitp.ac.in>
2023-09-10 17:28:32 +0530

    added basic frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import socketIO from "socket.io-client";
+
+const ENDPOINT = 'http://localhost:4500/';
+const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
+
+
+function App() {
+
+  socket.on("connect", () => {
+    // console.log("New Connection");
+  })
+
+  return (
+    <div className="App">
+      <h1>Working</h1>
+    </div>
+  );
+}
+
+export default App;
