b65fed238d3761267bf62670189305c9d86b53de
Pratik8696 <mepratikanand@gmail.com>
2023-09-10 17:46:35 +0530

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-import socketIO from "socket.io-client";
+// import socketIO from "socket.io-client";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Join from "./component/Join/Join";
 
-const ENDPOINT = 'http://localhost:4500/';
-const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
-
+// const ENDPOINT = 'http://localhost:4500/';
+// const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
 
 function App() {
-
-  socket.on("connect", () => {
-    // console.log("New Connection");
-  })
-
-  
   return (
     <div className="App">
-      <h1>Working</h1>
+      <Router>
+        <Route path="/" component={Join} />
+        <Route path="/chat" />
+      </Router>
     </div>
   );
 }
