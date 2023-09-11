a975afab7bee60b69f8d316cc0312426837e2a02
Pratik8696 <mepratikanand@gmail.com>
2023-09-11 15:24:58 +0530

    frontend final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
-// import socketIO from "socket.io-client";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Join from "./component/Join/Join";
 
-// const ENDPOINT = 'http://localhost:4500/';
-// const socket = socketIO(ENDPOINT, { transports: ['websocket'] });
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat"
+
+
+
 
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={Join} />
-        <Route path="/chat" />
-      </Router>
-    </div>
+
+
+    <Router>
+
+      <Route exact path="/" component={Join} />
+      <Route path="/chat" component={Chat} />
+
+    </Router>
+
   );
 }
 
--- frontend/src/component/Join/Join.js
+++ frontend/src/component/Join/Join.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react'
 import "./Join.css";
-import logo from "../../images/logo.png"
-function Join() {
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+
+let user;
+
+
+const sendUser = () => {
+    user = document.getElementById('joinInput').value;
+    document.getElementById('joinInput').value = "";
+}
+
+
+const Join = () => {
+
+    const [name, setname] = useState("");
+
     return (
-        <div className='JoinPage'>
-            <div className='JoinContainer'>
+        <div className="JoinPage">
+            <div className="JoinContainer">
                 <img src={logo} alt="logo" />
-                <h1>INFO RKC</h1>
-                <input type="text" id="JoinInput" />
-                <button className='joinbtn' >LogIn</button>
+                <h1>C CHAT</h1>
+                <input onChange={(e) => setname(e.target.value)} placeholder="Enter Your Name" type="text" id="joinInput" />
+                <Link onClick={(event) => !name ? event.preventDefault() : null} to="/chat">  <button onClick={sendUser} className="joinbtn">Login In</button></Link>
             </div>
         </div>
     )
 }
 
-export default Join;
+export default Join
+export { user }
